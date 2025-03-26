9720cdff3743478c7b567b889655348fb18bc783
tall_tom <s250441@dtu.dk>
2025-03-26 10:15:50 +0100

    Updated test

--- test.py
+++ test.py
@@ -1,10 +1,26 @@
 def compare(input1, input2):
-  try:
-    length1 = len(input1)
-    length2 = len(input2)
-  except   
+  '''Compares two inputs, outputs list of lists'''
+  alphabet = ['A','C','G','T']
+  sequences = [input1,input2]
+  
+  # Filtering inputs
+  for seq in sequences:
+    if type(seq) is not str:
+       return "Please input two strings for alignment"
+    seq = seq.upper()
+    for n in seq:
+      if n not in alphabet:
+        return "Please input a valid sequence"
+  
+  # Calculating length and building matrix
+  i = len(input1)
+  j = len(input2)
+  
+
+  
+print(compare('AG1CCA','AGGACT'))
 
 
 
-AGACCA
-AGGACT
+
+
