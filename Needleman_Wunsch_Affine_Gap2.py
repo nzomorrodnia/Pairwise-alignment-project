615557957a110fb99a482feaada0ad024d38da43
tall_tom <s250441@dtu.dk>
2025-05-04 16:10:58 +0200

    ...

--- Needleman_Wunsch_Affine_Gap2.py
+++ Needleman_Wunsch_Affine_Gap2.py
@@ -2,7 +2,7 @@ import numpy as np
 
 MATCH = 2
 MISMATCH = -1
-GAP_OPEN_PENALTY = -2
+GAP_OPEN_PENALTY = -3
 GAP_EXTEND_PENALTY = -1
 
 
@@ -43,32 +43,45 @@ def needleman_wunsch(seq1, seq2):
     m = len(seq2)
 
     score_matrix = np.zeros((n + 1, m + 1))
-    seq1_gaps_matrix = np.zeros((n + 1, m + 1))
-    seq2_gaps_matrix = np.zeros((n + 1, m + 1))
+    Q = np.zeros((n + 1, m + 1))
+    P = np.zeros((n + 1, m + 1))
 
     for i in range(1, n + 1):
-        seq1_gaps_matrix[i][0] = float('-INF')
-        score_matrix[i][0] = seq1_gaps_matrix[i][0]
+        Q[i][0] = float('-INF')
+        score_matrix[i][0] = GAP_OPEN_PENALTY + i*GAP_EXTEND_PENALTY
 
     for j in range(1, m + 1):
-        seq2_gaps_matrix[0][j] = float('-INF')
-        score_matrix[0][j] = seq2_gaps_matrix[0][j]
+        P[0][j] = float('-INF')
+        score_matrix[0][j] = GAP_OPEN_PENALTY + j*GAP_EXTEND_PENALTY
 
     for i in range(1, n + 1):
         for j in range(1, m + 1):
             score_match = score_matrix[i - 1][j - 1] + (MATCH if seq1[i - 1] == seq2[j - 1] else MISMATCH)
-            seq1_gaps_matrix[i][j] = max(seq1_gaps_matrix[i - 1][j] + GAP_EXTEND_PENALTY, score_matrix[i - 1][j] + GAP_OPEN_PENALTY)
-            seq2_gaps_matrix[i][j] = max(seq2_gaps_matrix[i][j - 1] + GAP_EXTEND_PENALTY, score_matrix[i][j - 1] + GAP_OPEN_PENALTY)
-            score_matrix[i][j] = max(score_match, seq1_gaps_matrix[i][j], seq2_gaps_matrix[i][j])
-
-    return traceback(seq1, seq2, score_matrix, seq1_gaps_matrix, seq2_gaps_matrix)
+            P[i][j] = max(P[i - 1][j] + GAP_EXTEND_PENALTY, score_matrix[i - 1][j] + GAP_OPEN_PENALTY + GAP_EXTEND_PENALTY)
+            Q[i][j] = max(Q[i][j - 1] + GAP_EXTEND_PENALTY, score_matrix[i][j - 1] + GAP_OPEN_PENALTY + GAP_EXTEND_PENALTY)
+            score_matrix[i][j] = max(score_match, P[i][j], Q[i][j])
+    for row in score_matrix:
+        print(row)
+    for row in P:
+        print(row)
+    for row in Q:
+        print(row)
+    return traceback(seq1, seq2, score_matrix, Q, P)
 
 
 def alignment_with_prints(seq1, seq2):
-    alignments = needleman_wunsch("GCATGCCAT", "CATGCATCGAC")
-    print("for: '" + seq1 + "' and: '" + seq2 + "'")
-    print("got: '" + alignments[0] + "' and: '" + alignments[1] + "'")
+    alignments = needleman_wunsch(seq1, seq2)
+    symbols = ""
+    for n in range(len(alignments[0])):
+        if alignments[0][n] == alignments[1][n]:
+            symbols += "|"
+        elif alignments[0][n] == '-' or alignments[1][n] == '-':
+            symbols += ' '
+        else:
+            symbols += '*' 
+    print(f"for: '{seq1} + ' and: ' + {seq2}'")
+    print(f"got:\n{alignments[0]}\n{symbols}\n{alignments[1]}")
     return alignments
 
 
-alignment_with_prints("GCATGCCAT", "CATGCATCGAC")
+alignment_with_prints("CG","CCGA")
